da99fb6e175886048b8c846589037f260347a5bb
Michael Kipfer <michael@kipfer-dp.com>
2026-06-20 22:00:10 +0200

    Saubere chronologische Reihenfolge (laufende zuerst), SW v3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker für "Eventi Lago Maggiore"
 // Macht die App offline-fähig (App-Hülle + letzte Eventdaten gecacht).
-const CACHE = 'eventi-v2';
+const CACHE = 'eventi-v3';
 const SHELL = [
   './', './index.html', './data.js',
   './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'
